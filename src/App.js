3115a18015778ac8b8861a157d49cd2b537de979
Alaska-999 <kristinagaranchuk@gmail.com>
2022-07-02 17:32:13 +0300

    Modal created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Modal from "./Modal/Modal";
+import {useState} from "react";
 
 function App() {
+  const [modalActive, setModalActive] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <button className='open-btn' onClick={() => setModalActive(true)}>Open modal window</button>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis consectetur culpa facilis labore perferendis perspiciatis, quia similique voluptate?</p>
+      </main>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis cum earum fugiat fugit harum laborum minus molestiae nihil nulla vero. Aspernatur cum, deleniti dolore harum iure laudantium sequi. Eligendi, reprehenderit!</p>
+      </Modal>
     </div>
   );
 }
